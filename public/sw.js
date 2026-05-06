28ab344880064a44b973567f5789c20ff51d835a
santjsx <heysanthoshreddy@gmail.com>
2026-05-06 20:59:30 +0530

    fix: book covers and enable service worker caching

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'archive-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.png',
+  'https://unpkg.com/lenis@1.1.14/dist/lenis.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        if (event.request.url.startsWith('http')) {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        }
+        return fetchResponse;
+      });
+    })
+  );
+});
